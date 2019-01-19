c079f50d4925e911594c41f9d9f4638b908ea798
andrewshayne <andrewshayne@gmail.com>
2019-01-19 11:04:37 -0600

    back at it

--- MenuState.cpp
+++ MenuState.cpp
@@ -5,12 +5,19 @@
 
 #include <iostream>
 
+
 MenuState::MenuState(Game &game, sf::RenderWindow &window) : GameState(game, window)
 {
 	font.loadFromFile("Fonts/BodoniFLF-Bold.ttf");
 	stateText.setCharacterSize(20);
 	stateText.setString("Hamster Game\n- Press space or enter");
 	stateText.setFont(font);
+
+
+	//search for save files
+	//push to list
+	//...
+	getSaveFiles();
 }
 
 
@@ -33,6 +40,24 @@ void MenuState::handlePlayerInputScroll(float scrollDelta, const sf::Vector2i mo
 {
 }
 
+void MenuState::getSaveFiles()
+{
+	fs::path root("./Savefiles");
+	fs::directory_iterator end_itr;
+
+	//cycle through directory
+	for (fs::directory_iterator itr(root); itr != end_itr; ++itr)
+	{
+		//if not a directory, list it
+		if (fs::is_regular_file(itr->path()))
+		{
+			std::string current_file = itr->path().string();
+			saveFiles.push_back(current_file);
+			//std::cout << current_file << std::endl;
+		}
+	}
+}
+
 void MenuState::update(sf::Time dt, const sf::Vector2i& mousePosition)
 {
 
@@ -47,4 +72,15 @@ void MenuState::draw()
 	sf::RenderWindow &window = *pWindow;
 	window.clear();
 	window.draw(stateText);
+
+	for (int i = 0; i < saveFiles.size(); i++)
+	{
+		sf::Text* text = new sf::Text();
+		text->setFont(font);
+		text->setString(std::to_string(i + 1) + ")\t" + saveFiles.at(i));
+		text->setCharacterSize(20);
+		text->setPosition({ 100.f, 100.f + (i * 40.f) });
+		text->setFillColor(sf::Color::White);
+		window.draw(*text);
+	}
 }
